a1d794122acfd81d9588d799eb0d54ae089eabf8
waterinocean <ajiql@126.com>
2022-12-30 07:53:29 +0800

    1. add catch2 library; 2. adjust file structure;

--- script/build_debug.sh
+++ script/build_debug.sh
@@ -1,4 +1,7 @@
+if [ ! -d build ]; then
+    mkdir build;
+fi
 
 cd build
-cmake .. -DCMAKE_BUILD_TYPE=Debug -G "MinGW Makefiles"
+cmake .. -DCMAKE_BUILD_TYPE=Debug -G "Unix Makefiles"
 cmake --build .
--- windows/file_tcp_server/main.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-
-
-int main(int argc, char* argv[])
-{
-	printf("Hello World!");
-	return 0;
-}
